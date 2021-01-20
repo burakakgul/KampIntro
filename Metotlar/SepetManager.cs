aceb786e247708287be00f81a989808c26a7d342
asus <asus@ASUSLAPTOPHBA>
2021-01-20 18:56:08 +0300

    Add project files.

---
+++ Metotlar/SepetManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlar
+{
+    class SepetManager
+    {
+        //naming convention, methodun büyük harfle başlaması
+        // syntax
+        //Ürünle ilgili her türlü işlem burada yapılır
+        public void Ekle(Urun urun)
+        {
+            Console.WriteLine("Tebrikler Sepete eklendi! : "+urun.Adi);
+        }
+
+        public void Ekle2(string urunAdi, string aciklama, double fiyat,int stokAdedi)
+        {
+            Console.WriteLine("Tebrikler, Sepete Eklendi: "+urunAdi);
+        }
+    }
+}

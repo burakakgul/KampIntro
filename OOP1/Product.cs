d22dc0f6947c7af750be09f8308db19fe1de218e
asus <asus@ASUSLAPTOPHBA>
2021-01-21 19:26:32 +0300

    20 Ocak Ders Sonu

---
+++ OOP1/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    //snippet
+    class Product
+    {
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        //ÜrünAdı
+        public string ProductName { get; set; }
+        //ÜrününBirimFiyatı
+        public double UnitPrice { get; set; }
+        //StokAdedi
+        public int UnitInStock { get; set; }
+
+        //CreatReadUpdateDelete - CRUD gibi sistemler için Class'lar kullanılır
+
+
+    }
+}

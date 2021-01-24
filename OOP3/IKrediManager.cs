247dc9ba954f661e51b48cfc3a5640b7cf6a4f27
asus <asus@ASUSLAPTOPHBA>
2021-01-24 13:13:19 +0300

    23 Ocak Ders Sonu

---
+++ OOP3/IKrediManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP3
+{
+    interface IKrediManager //interface olduğu anlaşılsın diye I ile başlar
+    {
+        void Hesapla();
+        void BiseyYap();
+    }
+}
---
+++ OOP3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //interface'ler new'lenemez..!
+            //Interface'lerde o interface'i implemente eden class'ın referans değerini tutuyor 
+            IKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
+            IKrediManager tasitKrediManager = new TasitKrediManager();
+            IKrediManager konutKrediManager = new KonutKrediManager();
+            IKrediManager esnafKrediManager = new EsnafKrediManager();
+
+            ILoggerService databaseLoggerService = new DatabaseLoggerService();
+            ILoggerService fileLoggerService = new FileLoggerService();
+            ILoggerService smsmLoggerService = new SmsLoggerService();
+
+            List<ILoggerService> loggers = new List<ILoggerService>() {smsmLoggerService , fileLoggerService };
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(esnafKrediManager, loggers );
+
+            //List'e eklenen hangi krediler olursa onların hesabı yapılır
+            List<IKrediManager> krediler = new List<IKrediManager>() {ihtiyacKrediManager,tasitKrediManager };
+
+            //basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+        }
+    }
+}
